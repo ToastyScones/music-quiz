a6fa7b61a301a3ac06ab4419a84c3942ec16957f
ToastyScones <94160275+ToastyScones@users.noreply.github.com>
2026-09-26 12:36:32 -0500

    Update with latest changes

--- scripts/index.js
+++ scripts/index.js
@@ -47,9 +47,8 @@ function createYtPlayer() {
       'controls': 0, // Show pause/play buttons in player
       'showinfo': 1, // Hide the video title
       'modestbranding': 1, // Hide the Youtube Logo'
-      'cc_load_policy': 0, // Hide closed captions
+      'cc_load_policy': 3, // Hide closed captions
       'iv_load_policy': 3 // Hide the Video Annotations
-      //'vq': 'medium'	  
     },
     events: {
       'onReady': onPlayerReady,
